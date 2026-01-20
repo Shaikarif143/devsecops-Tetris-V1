8110ba6dd5c1afefd4ee10dacf74171735025bc9
Shaik Arif <shaikmahammadarif98@gmail.com>
2026-01-20 18:53:47 +0530

    Update backend.tf

--- Jenkins-CICD/backend.tf
+++ Jenkins-CICD/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = var.bucket_name
-    key            = "devsecops-Tetris-V1/EKS_TERRAFORM/backend.tf"
+    key            = "devsecops-Tetris-V11"
     region         = var.aws_region
     dynamodb_table = var.dynamodb_table
   }
