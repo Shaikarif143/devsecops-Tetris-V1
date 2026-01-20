afc30a3a325a8f20466adc86e6b81a5a1bb49537
Shaik Arif <shaikmahammadarif98@gmail.com>
2026-01-20 18:30:45 +0530

    Update S3 backend key for Terraform configuration

--- Jenkins-CICD/backend.tf
+++ Jenkins-CICD/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = var.bucket_name
-    key            = "my-terraform-environment/main"
+    key            = "devsecops-Tetris-V1/EKS_TERRAFORM/backend.tf"
     region         = var.aws_region
     dynamodb_table = var.dynamodb_table
   }
